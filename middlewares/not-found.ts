d76e2e31156732cfd28b40517586f1dd0bf6c257
Alexandros Gialantzis <alexandrosalexandrosg@gmail.com>
2024-04-01 01:36:23 +0300

    log for url

--- middlewares/not-found.ts
+++ middlewares/not-found.ts
@@ -2,7 +2,6 @@ import { StatusCodes } from 'http-status-codes';
 import { Request, Response } from 'express';
 
 export const notFoundMiddleware = (req: Request, res: Response) => {
-  console.log(req.baseUrl, req.originalUrl);
   res
     .status(StatusCodes.NOT_FOUND)
     .json({ message: 'Δεν βρέθηκε η διαδρομή!' });
